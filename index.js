7eecd4e820468f6f643a77b420f9472bd48e39d4
Pruthviraj Jadhav <pruthvirajjadhav1@gmail.com>
2023-10-26 22:56:49 +0530

    feat: remove

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const axios = require('axios'); 
 const bodyParser = require('body-parser');
-const { Queue, Worker } = require('bullmq');
+const { Queue, Worker, Job } = require('bullmq');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,15 +12,45 @@ const myQueue = new Queue('myQueue', { connection: redisUrl, limiter: { max: 1, 
 
 const worker = new Worker('myQueue', async (job) => {
   console.log('Job executed at: ' + new Date().toISOString());
+  console.log('Cat Fact from API:', job.data); 
 });
 
-app.post('/create-job', (req, res) => {
-  const data = req.body;
+let jobID = 0;
 
-  const job = myQueue.add('logJob', data, { repeat: { every: 1000 } });
+app.post('/create-job', async (req, res) => {
+  try {
+    const apiResponse = await axios.get('https://catfact.ninja/fact');
 
-  res.status(200).json({ message: 'Job created successfully' });
+    const catFact = apiResponse.data.fact;
+
+    const job = await myQueue.add('logCatFact', catFact, { repeat: { every: 1000 } });
+    jobID = job.id;
+    res.status(200).json({ message: 'Cat Fact Job created successfully', ID: job.id });
+
+  } catch (error) {
+    console.error('Error fetching cat fact from the API:', error);
+    res.status(500).json({ error: 'Failed to fetch cat fact from the API' });
+  }
 });
+
+app.post('/remove-job', async (req, res) => {
+    const jobId = req.body.jobId; 
+  
+    try {
+      const job = await Job.fromId(myQueue, jobId);
+      if (job) {
+        await myQueue.obliterate();
+        res.status(200).json({ message: `Job with ID ${jobId} removed successfully` });
+      } else {
+        res.status(404).json({ error: 'Job not found' });
+      }
+    } catch (error) {
+      console.error('Error removing job:', error);
+      res.status(500).json({ error: 'Failed to remove the job' });
+    }
+  });
+  
+  
 
 const port = 3000;
 app.listen(port, () => {
