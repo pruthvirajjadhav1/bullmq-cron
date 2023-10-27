85fb77fb649327906cb11a2eef725b73dd9fdbc2
Pruthviraj Jadhav <pruthvirajjadhav1@gmail.com>
2023-10-27 15:29:58 +0530

    inital

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
 const express = require('express');
-const axios = require('axios'); 
-const bodyParser = require('body-parser');
-const { Queue, Worker, Job } = require('bullmq');
+const Bull = require('bull');
+const Arena = require('bull-arena');
+const axios = require('axios');
 
 const app = express();
-app.use(bodyParser.json());
+const port = 3000;
+app.use(express.json());
+// Create a Bull queue
+const catFactQueue = new Bull('catFactQueue');
 
-const redisUrl = 'redis://localhost:6379';
+// Worker 
+catFactQueue.process(async job => {
+  const response = await axios.get('https://catfact.ninja/fact');
+  const catFact = response.data.fact;
 
-const myQueue = new Queue('myQueue', { connection: redisUrl, limiter: { max: 1, duration: 1000 } });
-
-const worker = new Worker('myQueue', async (job) => {
-  console.log('Job executed at: ' + new Date().toISOString());
-  const apiResponse = await axios.get('https://catfact.ninja/fact');
-  job.data = apiResponse.data.fact;
-  console.log('Cat Fact from API:', job.data); 
+  console.log('Cat Fact:', catFact);
 });
 
-let jobID = 0;
+app.get('/create-job', async (req, res) => {
 
-app.post('/create-job', async (req, res) => {
-  try {
-    const apiResponse = await axios.get('https://catfact.ninja/fact');
-
-    const catFact = apiResponse.data.fact;
-
-    const job = await myQueue.add('logCatFact', catFact, { repeat: { every: 1000 }} );
-    jobID = job.id;
-    res.status(200).json({ message: 'Cat Fact Job created successfully', ID: job.id });
-
-  } catch (error) {
-    console.error('Error fetching cat fact from the API:', error);
-    res.status(500).json({ error: 'Failed to fetch cat fact from the API' });
-  }
+  const job = await catFactQueue.add({}, { repeat: { cron: '*/1 * * * * *' } });
+  const jobId = job.id;
+  res.json({message:"data added", id: jobId});
 });
 
 app.post('/remove-job', async (req, res) => {
-    const jobId = req.body.jobId; 
-  
-    try {
-      const job = await Job.fromId(myQueue, jobId);
-      if (job) {
-        await job.remove();
-        res.status(200).json({ message: `Job with ID ${jobId} removed successfully` });
-      } else {
-        res.status(404).json({ error: 'Job not found' });
-      }
-    } catch (error) {
-      console.error('Error removing job:', error);
-      res.status(500).json({ error: 'Failed to remove the job' });
-    }
-  });
-      
+  const jobId = req.body.jobId; 
 
-const port = 3000;
+  try {
+    const job = await catFactQueue.getJob(jobId);
+
+    if (job) {
+      await job.remove();
+      res.send(`Job with ID ${jobId} removed from the queue.`);
+    } else {
+      res.send(`Job with ID ${jobId} not found in the queue.`);
+    }
+  } catch (error) {
+    res.status(500).send(`Error removing job: ${error.message}`);
+  }
+});
+
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
